5783324aa3dab012dd377b5e6826f594d29dfd44
Marcoji <marcuoji@gmail.com>
2025-08-21 21:08:51 +0100

    Updated againss s.

--- server.js
+++ server.js
@@ -38,8 +38,8 @@ app.use(cors());
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
-app.use('/models', express.static(path.join(__dirname, '../frontend/models'))); // Serve face-api.js models
-app.use(express.static(path.join(__dirname, '../frontend')));
+app.use('/models', express.static(path.join(__dirname, '/frontend/models'))); // Serve face-api.js models
+app.use(express.static(path.join(__dirname, 'frontend')));
 // Request logging middleware
 app.use((req, res, next) => {
     log('info', `${req.method} ${req.path}`, {
